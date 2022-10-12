1c92797745b21701242c14f53308d5edfef54025
Jordan Harband <ljharb@gmail.com>
2022-10-11 21:40:59 -0700

    [eslint] fix indentation

--- test/parse.js
+++ test/parse.js
@@ -6,15 +6,15 @@ var json = require('../');
 var garbage = require('garbage');
 
 test('parse', function (t) {
-    for (var i = 0; i < 50; i++) {
-        var s = JSON.stringify(garbage(50));
+	for (var i = 0; i < 50; i++) {
+		var s = JSON.stringify(garbage(50));
 
-        t.deepEqual(
-            json.parse(s),
-            JSON.parse(s),
-            'comparing JSON.parse to jsonify.parse: run ' + (i + 1)
-        );
-    }
+		t.deepEqual(
+			json.parse(s),
+			JSON.parse(s),
+			'comparing JSON.parse to jsonify.parse: run ' + (i + 1)
+		);
+	}
 
-    t.end();
+	t.end();
 });
--- test/stringify.js
+++ test/stringify.js
@@ -5,14 +5,14 @@ var json = require('../');
 var garbage = require('garbage');
 
 test('stringify', function (t) {
-    for (var i = 0; i < 50; i++) {
-        var obj = garbage(50);
-        t.equal(
-            json.stringify(obj),
-            JSON.stringify(obj),
-            'comparing JSON.stringify to jsonify.stringify: run ' + (i + 1)
-        );
-    }
+	for (var i = 0; i < 50; i++) {
+		var obj = garbage(50);
+		t.equal(
+			json.stringify(obj),
+			JSON.stringify(obj),
+			'comparing JSON.stringify to jsonify.stringify: run ' + (i + 1)
+		);
+	}
 
-    t.end();
+	t.end();
 });
